ddf3254559c40e129fe9fd37f4c59281716e3f3a
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-22 14:59:26 -0300

    update mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
+
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}'é obrigatório."
 mongoose.Error.messages.Number.min = "O valor '{VALUE}' é menor que o limite minimo de '{MIN}'"
